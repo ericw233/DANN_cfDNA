ad2ea7e178df5a206c1ec4a305ea7bb6f6858d19
ericw233 <nanoeric2@gmail.com>
2024-03-28 16:57:26 -0400

    Updated Mar 28

--- All_feature_type_run_Mercury_domain0.sh
+++ All_feature_type_run_Mercury_domain0.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-feature_type_list=(AE AE_Frag AE_Arm AE_Cnv AE_Griffin Frag Arm Griffin Cnv MCMS Gemini Ma)
+feature_type_list=(VAE AE_Frag AE_Arm AE_Cnv AE_Griffin Frag Arm Griffin Cnv MCMS Gemini Ma)
 input_size=(550 64 64 64 64 1200 1000 2650 2550 350 120 50)
-output_path=/mnt/binf/eric/DANN_Jan2024Results_new/DANN_0227_AE
-data_dir=/mnt/binf/eric/Mercury_Dec2023/Feature_all_Dec2023_AE_v5.pkl
+output_path=/mnt/binf/eric/DANN_Mar2024Results/DANN_0321_VAE
+data_dir=/mnt/binf/eric/Mercury_Dec2023/Feature_all_Mar2024_VAE.pkl
 R01BTune=No
 cluster_method_list=(None kmeans DBSCAN GMM MeanShift)
 nfold=5
-for i in {5..11}
+for i in {0..0}
 do
     for j in {0..4}
     do
